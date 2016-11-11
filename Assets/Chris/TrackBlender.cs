6ddc23a086cb6d44c210c979577b68d226a75a4c
Chris Manthey <calined@gmx.de>
2016-11-11 14:23:08 +0100

    first commit

---
+++ Assets/Chris/TrackBlender.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class TrackBlender : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+    public AudioSource currentSource;
+    public AudioSource nextSource;
+    public Text currentTrackProgressTextObject;
+
+    public float currentFlightSpeed = 0f;
+
+    float currentTrackProgress = 0f;
+
+    public AudioClip[] trackList;
+
+    void nextTrack()
+    {
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentSource.clip = trackList[0];
+        nextSource.clip = trackList[1];
+        currentSource.Play();
+        nextSource.Play();
+
+        audiomixer.SetFloat("currentTrackVolume", 0f);
+        audiomixer.SetFloat("nextTrackVolume", -80f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTrackProgress += currentFlightSpeed * Time.deltaTime;
+        currentTrackProgressTextObject.text = "" + currentTrackProgress;
+
+        if (currentTrackProgress > 100f)
+        {
+            nextTrack();
+            currentTrackProgress = 0f;
+        }
+
+    }
+}
---
+++ Assets/Chris/drop_and_glide_blend.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class drop_and_glide_blend : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+    public Transform bird;
+
+    public Transform trackBlender;
+
+    // Use this for initialization
+    void Start()
+    {
+        adjustGlideAndDropBlend(0f);
+    }
+
+    public void adjustGlideAndDropBlend(float newValue)
+    {
+        bird.rotation = Quaternion.Euler(0f, 0f, newValue * 90f);
+        audiomixer.SetFloat("swooshEQGain", newValue);
+        audiomixer.SetFloat("musicEQGain", 1f - newValue);
+        audiomixer.SetFloat("swooshVolume", -10f + newValue * 10f);
+
+        trackBlender.GetComponent<TrackBlender>().currentFlightSpeed = newValue * 30f;
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
